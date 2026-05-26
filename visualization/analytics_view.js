83e55b61fd8c80e5fb5b798ce42108ae5334051d
Lorenzo <lory.pasi@gmail.com>
2026-05-26 12:37:33 +0200

    Fixed import problem

--- visualization/analytics_view.js
+++ visualization/analytics_view.js
@@ -4,7 +4,6 @@
  * ============================================================================
  */
 import {appState} from './state.js';
-import './plothandler.js';
 import {
     drawBenchmarkBarChart,
     drawCategoricalAssortativity,
@@ -19,21 +18,7 @@ import {
     drawRealVsErdosPercentile,
     drawTop10Horizontal,
     drawTripleCCDF
-} from "./plothandler";
-
-/**
- * Calculating factorial for Poisson Distribution
- * @param n - value to calculate the factorial of
- * @returns {number} The factorial of n
- */
-function factorial(n) {
-    if (n === 0 || n === 1) return 1;
-    let result = 1;
-    for (let i = 2; i <= n; i++) {
-        result *= i;
-    }
-    return result;
-}
+} from "./plothandler.js";
 
 /**
  * Renders analytics dashboards based on the selected view name. Clears existing graphical containers,
